3574ed2ca743fdcf809d37e52c700710c7c57b1c
DaffaPramudya <daffapramudya160605@gmail.com>
2025-05-14 21:01:35 +0700

    Task 4 upload

--- io_uring/rsrc.h
+++ io_uring/rsrc.h
@@ -5,50 +5,56 @@
 #include <linux/io_uring_types.h>
 #include <linux/lockdep.h>
 
-#define IO_VEC_CACHE_SOFT_CAP		256
+#define IO_VEC_CACHE_SOFT_CAP 256
 
-enum {
-	IORING_RSRC_FILE		= 0,
-	IORING_RSRC_BUFFER		= 1,
+enum
+{
+	IORING_RSRC_FILE = 0,
+	IORING_RSRC_BUFFER = 1,
 };
 
-struct io_rsrc_node {
-	unsigned char			type;
-	int				refs;
+struct io_rsrc_node
+{
+	unsigned char type;
+	int refs;
 
 	u64 tag;
-	union {
+	union
+	{
 		unsigned long file_ptr;
 		struct io_mapped_ubuf *buf;
 	};
 };
 
-enum {
-	IO_IMU_DEST	= 1 << ITER_DEST,
-	IO_IMU_SOURCE	= 1 << ITER_SOURCE,
+enum
+{
+	IO_IMU_DEST = 1 << ITER_DEST,
+	IO_IMU_SOURCE = 1 << ITER_SOURCE,
 };
 
-struct io_mapped_ubuf {
-	u64		ubuf;
-	unsigned int	len;
-	unsigned int	nr_bvecs;
-	unsigned int    folio_shift;
-	refcount_t	refs;
-	unsigned long	acct_pages;
-	void		(*release)(void *);
-	void		*priv;
-	bool		is_kbuf;
-	u8		dir;
-	struct bio_vec	bvec[] __counted_by(nr_bvecs);
+struct io_mapped_ubuf
+{
+	u64 ubuf;
+	unsigned int len;
+	unsigned int nr_bvecs;
+	unsigned int folio_shift;
+	refcount_t refs;
+	unsigned long acct_pages;
+	void (*release)(void *);
+	void *priv;
+	bool is_kbuf;
+	u8 dir;
+	struct bio_vec bvec[] __counted_by(nr_bvecs);
 };
 
-struct io_imu_folio_data {
+struct io_imu_folio_data
+{
 	/* Head folio can be partially included in the fixed buf */
-	unsigned int	nr_pages_head;
+	unsigned int nr_pages_head;
 	/* For non-head/tail folios, has to be fully included */
-	unsigned int	nr_pages_mid;
-	unsigned int	folio_shift;
-	unsigned int	nr_folios;
+	unsigned int nr_pages_mid;
+	unsigned int folio_shift;
+	unsigned int nr_folios;
 };
 
 bool io_rsrc_cache_init(struct io_ring_ctx *ctx);
@@ -59,35 +65,109 @@ void io_rsrc_data_free(struct io_ring_ctx *ctx, struct io_rsrc_data *data);
 int io_rsrc_data_alloc(struct io_rsrc_data *data, unsigned nr);
 
 struct io_rsrc_node *io_find_buf_node(struct io_kiocb *req,
-				      unsigned issue_flags);
+									  unsigned issue_flags);
 int io_import_reg_buf(struct io_kiocb *req, struct iov_iter *iter,
-			u64 buf_addr, size_t len, int ddir,
-			unsigned issue_flags);
+					  u64 buf_addr, size_t len, int ddir,
+					  unsigned issue_flags);
 int io_import_reg_vec(int ddir, struct iov_iter *iter,
-			struct io_kiocb *req, struct iou_vec *vec,
-			unsigned nr_iovs, unsigned issue_flags);
+					  struct io_kiocb *req, struct iou_vec *vec,
+					  unsigned nr_iovs, unsigned issue_flags);
 int io_prep_reg_iovec(struct io_kiocb *req, struct iou_vec *iv,
-			const struct iovec __user *uvec, size_t uvec_segs);
+					  const struct iovec __user *uvec, size_t uvec_segs);
 
 int io_register_clone_buffers(struct io_ring_ctx *ctx, void __user *arg);
 int io_sqe_buffers_unregister(struct io_ring_ctx *ctx);
 int io_sqe_buffers_register(struct io_ring_ctx *ctx, void __user *arg,
-			    unsigned int nr_args, u64 __user *tags);
+							unsigned int nr_args, u64 __user *tags);
 int io_sqe_files_unregister(struct io_ring_ctx *ctx);
 int io_sqe_files_register(struct io_ring_ctx *ctx, void __user *arg,
-			  unsigned nr_args, u64 __user *tags);
+						  unsigned nr_args, u64 __user *tags);
 
 int io_register_files_update(struct io_ring_ctx *ctx, void __user *arg,
-			     unsigned nr_args);
+							 unsigned nr_args);
+/**
+ * io_register_rsrc_update - Updates registered resources in the io_uring context.
+ *
+ * @ctx: Pointer to the io_ring_ctx structure representing the io_uring context.
+ * @arg: Pointer to the user-provided update data.
+ * @size: Size of the update data structure.
+ * @type: The type of resource to update (e.g., files or buffers).
+ *
+ * This function updates the registered resources in the io_uring context based
+ * on the user-provided update data. It validates the input and invokes the
+ * appropriate resource update handler.
+ *
+ * Returns:
+ * - The number of successfully updated resources on success.
+ * - Negative error code on failure.
+ */
 int io_register_rsrc_update(struct io_ring_ctx *ctx, void __user *arg,
-			    unsigned size, unsigned type);
+							unsigned size, unsigned type);
+
+/**
+ * io_register_rsrc - Registers resources (files or buffers) for io_uring.
+ *
+ * @ctx: Pointer to the io_ring_ctx structure representing the io_uring context.
+ * @arg: Pointer to the user-provided resource registration data.
+ * @size: Size of the resource registration data structure.
+ * @type: The type of resource to register (e.g., files or buffers).
+ *
+ * This function handles the registration of resources, such as files or buffers,
+ * for use with io_uring. It validates the input parameters, allocates the necessary
+ * resources, and updates the io_uring context with the registered resources.
+ *
+ * Returns:
+ * - 0 on success.
+ * - Negative error code on failure.
+ */
 int io_register_rsrc(struct io_ring_ctx *ctx, void __user *arg,
-			unsigned int size, unsigned int type);
+					 unsigned int size, unsigned int type);
+
+/**
+ * io_buffer_validate - Validates a user-provided buffer.
+ *
+ * @iov: Pointer to the iovec structure representing the buffer.
+ *
+ * This function checks if the provided buffer is valid, ensuring that
+ * it meets the constraints for size, alignment, and address range.
+ *
+ * Returns:
+ * - 0 on success.
+ * - Negative error code on invalid input.
+ */
 int io_buffer_validate(struct iovec *iov);
 
+/**
+ * io_check_coalesce_buffer - Checks if a buffer can be coalesced.
+ *
+ * @page_array: Array of pages representing the buffer.
+ * @nr_pages: Number of pages in the buffer.
+ * @data: Pointer to the io_imu_folio_data structure containing folio details.
+ *
+ * This function checks if the given buffer can be coalesced into a single
+ * contiguous memory region based on the provided folio data.
+ *
+ * Returns:
+ * - true if the buffer can be coalesced.
+ * - false otherwise.
+ */
 bool io_check_coalesce_buffer(struct page **page_array, int nr_pages,
-			      struct io_imu_folio_data *data);
+							  struct io_imu_folio_data *data);
 
+/**
+ * io_rsrc_node_lookup - Looks up a resource node by index.
+ *
+ * @data: Pointer to the io_rsrc_data structure containing resource nodes.
+ * @index: Index of the resource node to look up.
+ *
+ * This function retrieves a resource node from the resource data structure
+ * based on the specified index. It ensures that the index is within bounds
+ * and uses array_index_nospec to prevent speculative execution attacks.
+ *
+ * Returns:
+ * - Pointer to the resource node on success.
+ * - NULL if the index is out of bounds.
+ */
 static inline struct io_rsrc_node *io_rsrc_node_lookup(struct io_rsrc_data *data,
 						       int index)
 {
@@ -142,10 +222,46 @@ static inline void io_req_assign_buf_node(struct io_kiocb *req,
 }
 
 int io_files_update(struct io_kiocb *req, unsigned int issue_flags);
+
+/**
+ * io_files_update_prep - Prepares a file update request for io_uring.
+ *
+ * @req: Pointer to the io_kiocb structure representing the request.
+ * @sqe: Pointer to the io_uring submission queue entry.
+ *
+ * This function initializes the necessary parameters for updating files
+ * in the io_uring context based on the submission queue entry (SQE).
+ *
+ * Returns:
+ * - 0 on success.
+ * - Negative error code on invalid input.
+ */
 int io_files_update_prep(struct io_kiocb *req, const struct io_uring_sqe *sqe);
 
+/**
+ * __io_account_mem - Accounts memory usage for a user.
+ *
+ * @user: Pointer to the user structure.
+ * @nr_pages: Number of pages to account.
+ *
+ * This function checks if the user has sufficient memory quota to account
+ * for the specified number of pages. If the quota is exceeded, it returns
+ * an error. Otherwise, it updates the user's locked memory count.
+ *
+ * Returns:
+ * - 0 on success.
+ * - -ENOMEM if the memory limit is exceeded.
+ */
 int __io_account_mem(struct user_struct *user, unsigned long nr_pages);
 
+/**
+ * __io_unaccount_mem - Unaccounts memory usage for a user.
+ *
+ * @user: Pointer to the user structure.
+ * @nr_pages: Number of pages to unaccount.
+ *
+ * This function decreases the memory usage count for the given user.
+ */
 static inline void __io_unaccount_mem(struct user_struct *user,
 				      unsigned long nr_pages)
 {
@@ -153,8 +269,33 @@ static inline void __io_unaccount_mem(struct user_struct *user,
 }
 
 void io_vec_free(struct iou_vec *iv);
+
+/**
+ * io_vec_realloc - Reallocates an iovec structure.
+ *
+ * @iv: Pointer to the iou_vec structure to be reallocated.
+ * @nr_entries: The new number of entries for the iovec structure.
+ *
+ * This function reallocates the memory for the iovec structure to accommodate
+ * the specified number of entries. If the reallocation is successful, the
+ * existing iovec structure is updated with the new memory.
+ *
+ * Returns:
+ * - 0 on success.
+ * - Negative error code on failure.
+ */
 int io_vec_realloc(struct iou_vec *iv, unsigned nr_entries);
 
+/**
+ * io_vec_reset_iovec - Resets an iovec structure.
+ *
+ * @iv: Pointer to the iou_vec structure to be reset.
+ * @iovec: Pointer to the new iovec array.
+ * @nr: Number of entries in the new iovec array.
+ *
+ * This function resets the iovec structure by freeing its existing memory
+ * and assigning the new iovec array.
+ */
 static inline void io_vec_reset_iovec(struct iou_vec *iv,
 				      struct iovec *iovec, unsigned nr)
 {
--- io_uring/slist.h
+++ io_uring/slist.h
@@ -3,117 +3,246 @@
 
 #include <linux/io_uring_types.h>
 
+/**
+ * __wq_list_for_each - Iterates over each node in a work queue list.
+ *
+ * @pos: Pointer to the current node in the iteration.
+ * @head: Pointer to the head of the work queue list.
+ *
+ * This macro iterates over each node in the work queue list starting from
+ * the first node.
+ */
 #define __wq_list_for_each(pos, head)				\
-	for (pos = (head)->first; pos; pos = (pos)->next)
+    for (pos = (head)->first; pos; pos = (pos)->next)
 
+/**
+ * wq_list_for_each - Iterates over each node in a work queue list with a previous pointer.
+ *
+ * @pos: Pointer to the current node in the iteration.
+ * @prv: Pointer to the previous node in the iteration.
+ * @head: Pointer to the head of the work queue list.
+ *
+ * This macro iterates over each node in the work queue list while keeping
+ * track of the previous node.
+ */
 #define wq_list_for_each(pos, prv, head)			\
-	for (pos = (head)->first, prv = NULL; pos; prv = pos, pos = (pos)->next)
+    for (pos = (head)->first, prv = NULL; pos; prv = pos, pos = (pos)->next)
 
+/**
+ * wq_list_for_each_resume - Resumes iteration over a work queue list.
+ *
+ * @pos: Pointer to the current node in the iteration.
+ * @prv: Pointer to the previous node in the iteration.
+ *
+ * This macro resumes iteration over a work queue list from the current
+ * position.
+ */
 #define wq_list_for_each_resume(pos, prv)			\
-	for (; pos; prv = pos, pos = (pos)->next)
+    for (; pos; prv = pos, pos = (pos)->next)
 
+/**
+ * wq_list_empty - Checks if a work queue list is empty.
+ *
+ * @list: Pointer to the work queue list.
+ *
+ * Returns:
+ * - true if the list is empty.
+ * - false otherwise.
+ */
 #define wq_list_empty(list)	(READ_ONCE((list)->first) == NULL)
 
+/**
+ * INIT_WQ_LIST - Initializes a work queue list.
+ *
+ * @list: Pointer to the work queue list to be initialized.
+ *
+ * This macro initializes a work queue list by setting its first node to NULL.
+ */
 #define INIT_WQ_LIST(list)	do {				\
-	(list)->first = NULL;					\
+    (list)->first = NULL;					\
 } while (0)
 
+/**
+ * wq_list_add_after - Adds a node after a specified position in a work queue list.
+ *
+ * @node: Pointer to the node to be added.
+ * @pos: Pointer to the position after which the node will be added.
+ * @list: Pointer to the work queue list.
+ *
+ * This function inserts a node after the specified position in the work queue list.
+ */
 static inline void wq_list_add_after(struct io_wq_work_node *node,
-				     struct io_wq_work_node *pos,
-				     struct io_wq_work_list *list)
+                     struct io_wq_work_node *pos,
+                     struct io_wq_work_list *list)
 {
-	struct io_wq_work_node *next = pos->next;
+    struct io_wq_work_node *next = pos->next;
 
-	pos->next = node;
-	node->next = next;
-	if (!next)
-		list->last = node;
+    pos->next = node;
+    node->next = next;
+    if (!next)
+        list->last = node;
 }
 
+/**
+ * wq_list_add_tail - Adds a node to the tail of a work queue list.
+ *
+ * @node: Pointer to the node to be added.
+ * @list: Pointer to the work queue list.
+ *
+ * This function appends a node to the end of the work queue list.
+ */
 static inline void wq_list_add_tail(struct io_wq_work_node *node,
-				    struct io_wq_work_list *list)
+                    struct io_wq_work_list *list)
 {
-	node->next = NULL;
-	if (!list->first) {
-		list->last = node;
-		WRITE_ONCE(list->first, node);
-	} else {
-		list->last->next = node;
-		list->last = node;
-	}
+    node->next = NULL;
+    if (!list->first) {
+        list->last = node;
+        WRITE_ONCE(list->first, node);
+    } else {
+        list->last->next = node;
+        list->last = node;
+    }
 }
 
+/**
+ * wq_list_add_head - Adds a node to the head of a work queue list.
+ *
+ * @node: Pointer to the node to be added.
+ * @list: Pointer to the work queue list.
+ *
+ * This function prepends a node to the beginning of the work queue list.
+ */
 static inline void wq_list_add_head(struct io_wq_work_node *node,
-				    struct io_wq_work_list *list)
+                    struct io_wq_work_list *list)
 {
-	node->next = list->first;
-	if (!node->next)
-		list->last = node;
-	WRITE_ONCE(list->first, node);
+    node->next = list->first;
+    if (!node->next)
+        list->last = node;
+    WRITE_ONCE(list->first, node);
 }
 
+/**
+ * wq_list_cut - Cuts a segment from a work queue list.
+ *
+ * @list: Pointer to the work queue list.
+ * @last: Pointer to the last node in the segment to be cut.
+ * @prev: Pointer to the node preceding the segment to be cut.
+ *
+ * This function removes a segment of nodes from the work queue list.
+ */
 static inline void wq_list_cut(struct io_wq_work_list *list,
-			       struct io_wq_work_node *last,
-			       struct io_wq_work_node *prev)
+                   struct io_wq_work_node *last,
+                   struct io_wq_work_node *prev)
 {
 	/* first in the list, if prev==NULL */
-	if (!prev)
-		WRITE_ONCE(list->first, last->next);
-	else
-		prev->next = last->next;
+    if (!prev)
+        WRITE_ONCE(list->first, last->next);
+    else
+        prev->next = last->next;
 
-	if (last == list->last)
-		list->last = prev;
-	last->next = NULL;
+    if (last == list->last)
+        list->last = prev;
+    last->next = NULL;
 }
 
+/**
+ * __wq_list_splice - Splices a work queue list into another list.
+ *
+ * @list: Pointer to the work queue list to be spliced.
+ * @to: Pointer to the node where the list will be spliced.
+ *
+ * This function appends the nodes of one work queue list to another.
+ */
 static inline void __wq_list_splice(struct io_wq_work_list *list,
-				    struct io_wq_work_node *to)
+                    struct io_wq_work_node *to)
 {
-	list->last->next = to->next;
-	to->next = list->first;
-	INIT_WQ_LIST(list);
+    list->last->next = to->next;
+    to->next = list->first;
+    INIT_WQ_LIST(list);
 }
 
+/**
+ * wq_list_splice - Splices a work queue list into another list if not empty.
+ *
+ * @list: Pointer to the work queue list to be spliced.
+ * @to: Pointer to the node where the list will be spliced.
+ *
+ * Returns:
+ * - true if the list was spliced successfully.
+ * - false if the list was empty.
+ */
 static inline bool wq_list_splice(struct io_wq_work_list *list,
-				  struct io_wq_work_node *to)
+                  struct io_wq_work_node *to)
 {
-	if (!wq_list_empty(list)) {
-		__wq_list_splice(list, to);
-		return true;
-	}
-	return false;
+    if (!wq_list_empty(list)) {
+        __wq_list_splice(list, to);
+        return true;
+    }
+    return false;
 }
 
+/**
+ * wq_stack_add_head - Adds a node to the head of a stack.
+ *
+ * @node: Pointer to the node to be added.
+ * @stack: Pointer to the stack.
+ *
+ * This function pushes a node onto the stack.
+ */
 static inline void wq_stack_add_head(struct io_wq_work_node *node,
-				     struct io_wq_work_node *stack)
+                     struct io_wq_work_node *stack)
 {
-	node->next = stack->next;
-	stack->next = node;
+    node->next = stack->next;
+    stack->next = node;
 }
 
+/**
+ * wq_list_del - Deletes a node from a work queue list.
+ *
+ * @list: Pointer to the work queue list.
+ * @node: Pointer to the node to be deleted.
+ * @prev: Pointer to the node preceding the node to be deleted.
+ *
+ * This function removes a node from the work queue list.
+ */
 static inline void wq_list_del(struct io_wq_work_list *list,
-			       struct io_wq_work_node *node,
-			       struct io_wq_work_node *prev)
+                   struct io_wq_work_node *node,
+                   struct io_wq_work_node *prev)
 {
-	wq_list_cut(list, node, prev);
+    wq_list_cut(list, node, prev);
 }
 
+/**
+ * wq_stack_extract - Extracts a node from the head of a stack.
+ *
+ * @stack: Pointer to the stack.
+ *
+ * Returns:
+ * - Pointer to the extracted node.
+ */
 static inline
 struct io_wq_work_node *wq_stack_extract(struct io_wq_work_node *stack)
 {
-	struct io_wq_work_node *node = stack->next;
+    struct io_wq_work_node *node = stack->next;
 
-	stack->next = node->next;
-	return node;
+    stack->next = node->next;
+    return node;
 }
 
+/**
+ * wq_next_work - Retrieves the next work item in a work queue.
+ *
+ * @work: Pointer to the current work item.
+ *
+ * Returns:
+ * - Pointer to the next work item, or NULL if there are no more items.
+ */
 static inline struct io_wq_work *wq_next_work(struct io_wq_work *work)
 {
-	if (!work->list.next)
-		return NULL;
+    if (!work->list.next)
+        return NULL;
 
-	return container_of(work->list.next, struct io_wq_work, list);
+    return container_of(work->list.next, struct io_wq_work, list);
 }
 
 #endif // INTERNAL_IO_SLIST_H
